8b76d85c406dec1a116c14a69a80ec7200663196
mightysai1997 <110396758+mightysai1997@users.noreply.github.com>
2024-08-29 17:43:00 +0530

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,35 +1,23 @@
-import express, { Request, Response } from 'express';
+// CWE-312: Cleartext Storage of Sensitive Information
+// Description: This code stores sensitive information (username and password) in cleartext 
+// using localStorage. This approach is insecure because localStorage is accessible by any
+// script running on the page and can be exploited if an attacker gains access to the browser.
 
-const app = express();
+class User {
+    username: string;
+    password: string;
 
-// Middleware to parse URL-encoded bodies
-app.use(express.urlencoded({ extended: true }));
+    constructor(username: string, password: string) {
+        this.username = username;
+        this.password = password;
+    }
 
-app.get('/remember-password', (req: Request, res: Response) => {
-    // Retrieving sensitive data from query parameters
-    const pw = req.query.current_password as string;
+    saveCredentials() {
+        // BAD: Storing sensitive information (username and password) in cleartext
+        localStorage.setItem('username', this.username); // Sensitive data stored in localStorage
+        localStorage.setItem('password', this.password); // Sensitive data stored in localStorage
+    }
+}
 
-    // CWE-312: Cleartext Storage of Sensitive Information
-    // Description: This code stores sensitive information (password) in a cookie
-    // without encryption or protection. This makes it vulnerable to exposure 
-    // if the cookie is intercepted or accessed by unauthorized parties.
-    //
-    // Issues:
-    // - The cookie is set with cleartext sensitive data.
-    // - No security attributes like `httpOnly` or `secure` are applied.
-    //
-    // Risks:
-    // - Sensitive information (password) can be exposed if intercepted or accessed.
-    // - The cookie can be read by client-side scripts if `httpOnly` is not set.
-    // - Data in the cookie can be read over insecure connections if `secure` is not set.
-
-    res.cookie("password", pw);
-
-    res.send('Password cookie has been set');
-});
-
-// Start the server
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+const user = new User('exampleUser', 'examplePassword');
+user.saveCredentials();
